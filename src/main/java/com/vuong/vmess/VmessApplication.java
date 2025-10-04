f64112140ec543a6360abe86dcc0aa3cd9a25aa0
Vuong <vanvuong2005827@gmail.com>
2025-10-04 19:21:47 +0700

    first commit

---
+++ src/main/java/com/vuong/vmess/VmessApplication.java
@@ -0,0 +1,13 @@
+package com.vuong.vmess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vuong/vmess/VmessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuong.vmess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
